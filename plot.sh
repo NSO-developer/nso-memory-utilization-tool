025ddba1589be75aca6b8de13cd36a08debdc110
Mikael Mollberg <mmollber@cisco.com>
2025-10-17 13:10:28 +0200

    Add backslash interpretation with -e to echo

--- plot.sh
+++ plot.sh
@@ -5,20 +5,20 @@ bash collect.sh ncs.smp $1 &
 bash collect.sh NcsJVMLauncher $1 &
 bash collect.sh python3 $1 &
 wait
-echo "===================================== Collection for for all process done  =================================================\n\n"
+echo -e "===================================== Collection for for all process done  =================================================\n\n"
 
 
 echo "====================================== Ploting graph to all process ========================================================"
 echo "====================================== Ploting graph for ncs.smp process ========================================================"
 bash graphs.sh ncs.smp
-echo "===================================== Ploting graph for ncs.smp process done  =================================================\n"
+echo -e "===================================== Ploting graph for ncs.smp process done  =================================================\n"
 echo "====================================== Ploting graph for NcsJVMLauncher process ========================================================"
 bash graphs.sh NcsJVMLauncher
-echo "===================================== Ploting graph for NcsJVMLauncher process done  =================================================\n"
+echo -e "===================================== Ploting graph for NcsJVMLauncher process done  =================================================\n"
 echo "====================================== Ploting graph for python3 process ========================================================"
 bash graphs.sh python3
-echo "===================================== Ploting graph for python3 process done  =================================================\n"
+echo -e "===================================== Ploting graph for python3 process done  =================================================\n"
 echo "====================================== Ploting graph to compare between process ========================================================"
 bash graphs_compare.sh
-echo "====================================== Ploting graph to compare between process done ========================================================\n"
+echo -e "====================================== Ploting graph to compare between process done ========================================================\n"
 echo "===================================== Ploting graph to all process done  ================================================="
